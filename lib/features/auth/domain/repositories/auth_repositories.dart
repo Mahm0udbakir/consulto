ba4ef782500c6c4dd4449249afa8b659ac4e7ac2
Mahmoud Beeko <mahmoud.m.bakir@gmail.com>
2025-03-05 16:31:21 +0200

    Modified auth_repositories file - Added dartz package - Used dartz to return either failure or user data in sign-up and sign-in

--- lib/features/auth/domain/repositories/auth_repositories.dart
+++ lib/features/auth/domain/repositories/auth_repositories.dart
@@ -1,8 +1,12 @@
+import 'package:consulto/core/errors/failure.dart';
+import 'package:dartz/dartz.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 abstract class AuthRepository {
-  Future<User?> signUp(String email, String password);
-  Future<User?> signIn(String email, String password);
+  Future<Either<Failure,User?>> signUp(String email, String password);
+  Future<Either<Failure,User?>> signIn(String email, String password);
   Future<void> signOut();
   Stream<User?> get userStream;
 }
+
+//! The Either is from dartz package which allows two return types
